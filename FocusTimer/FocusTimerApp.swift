b241c79972120a4fc0ba1bcc08b3f25aebfe2cfd
Dilipan Prabha <dilipanprabha@Dilipans-MacBook-Air.local>
2025-07-26 09:59:23 +0530

    initial commit

---
+++ FocusTimer/FocusTimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerApp.swift
+//  FocusTimer
+//
+//  Created by Dilipan Prabha on 26/07/25.
+//
+
+import SwiftUI
+
+@main
+struct FocusTimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ FocusTimerTests/FocusTimerTests.swift
@@ -0,0 +1,17 @@
+//
+//  FocusTimerTests.swift
+//  FocusTimerTests
+//
+//  Created by Dilipan Prabha on 26/07/25.
+//
+
+import Testing
+@testable import FocusTimer
+
+struct FocusTimerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
